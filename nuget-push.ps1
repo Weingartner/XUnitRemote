bab6260a909c4873b1556c805b1e2bdf082d9474
bradphelan <b.phelan@weingartner.com>
2016-04-06 08:43:33 +0200

    fix up nuget script

--- nuget-push.ps1
+++ nuget-push.ps1
@@ -1,4 +1,5 @@
 param([string]$apikey) 
+rm *.nupkg
 .\NuGet.exe pack -symbols .\XUnitRemote\XUnitRemote.csproj
-$pkg = ls .\*.nupkg | sort-object | where { !$_.FullName.Contains(".symbols.") } | select-object -last 1
+$pkg = ls .\*.nupkg | where { !$_.FullName.Contains(".symbols.") } | select-object -last 1
 ./nuget.exe push $pkg.FullName  -ApiKey $apikey
