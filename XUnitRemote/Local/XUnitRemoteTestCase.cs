b78e11dfd394abafee6aac9cfbd4c181c72d99f2
Johannes Egger <eggerjohansi@gmail.com>
2016-06-13 07:06:57 +0200

    Add serialization support to `XUnitRemoteTestCase`.
    The VS runner doesn't work without it.

--- XUnitRemote/Local/XUnitRemoteTestCase.cs
+++ XUnitRemote/Local/XUnitRemoteTestCase.cs
@@ -9,15 +9,36 @@ namespace XUnitRemote.Local
 {
     public class XUnitRemoteTestCase : XunitTestCase
     {
-        private readonly string _Id;
-        private readonly string _ExePath;
+        private string _Id;
+        private string _ExePath;
 
         [EditorBrowsable(EditorBrowsableState.Never)]
         [Obsolete("Called by the de-serializer; should only be called by deriving classes for de-serialization purposes")]
         public XUnitRemoteTestCase() { }
 
+        public override void Deserialize(IXunitSerializationInfo data)
+        {
+            base.Deserialize(data);
+            Initialize(data.GetValue<string>("Id"), data.GetValue<string>("ExePath"));
+        }
+
+        public override void Serialize(IXunitSerializationInfo data)
+        {
+            base.Serialize(data);
+            data.AddValue("Id", _Id);
+            data.AddValue("ExePath", _ExePath);
+        }
+
+        protected override string GetUniqueID()
+            => $"{base.GetUniqueID()} [{_Id}, {_ExePath}]";
+
         public XUnitRemoteTestCase(IMessageSink diagnosticMessageSink, TestMethodDisplay defaultMethodDisplay, ITestMethod testMethod, string id, string exePath)
             : base(diagnosticMessageSink, defaultMethodDisplay, testMethod)
+        {
+            Initialize(id, exePath);
+        }
+
+        private void Initialize(string id, string exePath)
         {
             _Id = id;
             _ExePath = exePath;
