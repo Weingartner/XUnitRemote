2c02ce1e1eb886076828aab7b3d3f05a4a916dc2
Johannes Egger <eggerjohansi@gmail.com>
2016-06-13 11:52:15 +0200

    Switch to PackProj

--- nuget-push.ps1
+++
@@ -1,5 +0,0 @@
-param([string]$apikey, [string]$configuration="Debug") 
-rm *.nupkg
-.\NuGet.exe pack -Prop Platform=AnyCPU -Prop Configuration=$configuration -symbols .\XUnitRemote\XUnitRemote.csproj
-$pkg = ls .\*.nupkg | where { !$_.FullName.Contains(".symbols.") } | select-object -last 1
-./nuget.exe push $pkg.FullName  -ApiKey $apikey
